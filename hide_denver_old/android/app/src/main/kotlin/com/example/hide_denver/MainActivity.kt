90cc5bd5c9f5053754e973d2e938d5136f3f296e
Fleench <social.glenn@flench.me>
2026-05-18 16:05:54 -0600

    m

--- hide_denver_old/android/app/src/main/kotlin/com/example/hide_denver/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hide_denver
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- hide_denver_old/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hide_denver/main.dart';
-
-void main() {
-  test('app widget is constructible', () {
-    expect(const MissionCompanionApp(), isA<MissionCompanionApp>());
-  });
-}
